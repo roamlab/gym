019e2bcfcc39fc13dbccfc94bfa588da296adbf8
gagan <gk2496@columbia.edu>
2019-06-18 11:06:22 -0400

    added DictShapeWrapper

--- gym/wrappers/dict.py
+++ gym/wrappers/dict.py
@@ -26,3 +26,21 @@ class FlattenDictWrapper(gym.ObservationWrapper):
         for key in self.dict_keys:
             obs.append(observation[key].ravel())
         return np.concatenate(obs)
+
+
+class DictShapeWrapper(gym.ObservationWrapper):
+    """Add shape to Dict observation space."""
+
+    def __init__(self, env, dict_keys):
+        super(DictShapeWrapper, self).__init__(env)
+        self.dict_keys = dict_keys
+
+        # Figure out observation_space dimension.
+        size = 0
+        for key in dict_keys:
+            shape = self.env.observation_space.spaces[key].shape
+            size += np.prod(shape)
+        self.observation_space.shape = (size, )
+
+    def observation(self, observation):
+        return observation
